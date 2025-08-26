8225d451acfe69ce2953524596cb965c847d7bce
MrDHiggins <dyllanhiggins7@gmail.com>
2025-08-26 21:39:38 +0200

    implement main function to initialize router and session handling

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/MrDHiggins/planning-poker-backend/internal/api"
+	"github.com/MrDHiggins/planning-poker-backend/internal/service"
+	"github.com/MrDHiggins/planning-poker-backend/internal/store/memory"
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	sessionStore := memory.NewSessionMemoryStore()
+	sessionService := service.NewSessionService(sessionStore)
+	sessionHandler := api.NewSessionHandler(sessionService)
+
+	sessionHandler.RegisterRoutes(r)
+
+	log.Println("Planning Poker backend started on :8080")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
+	}
+}
