a7d9485d99a419cef247abf28974a3d2a140ad12
MrDHiggins <dyllanhiggins7@gmail.com>
2025-09-07 03:01:36 +0200

    add Votes field to Session model and implement Vote model

--- internal/models/session.go
+++ internal/models/session.go
@@ -6,6 +6,7 @@ type Session struct {
 	ID           string                  `json:"id"`
 	CreatedAt    time.Time               `json:"created_at"`
 	HostId       string                  `json:"host_id"`
-	Ticket       string                  `json:"ticket,omitempty"` //Omitted for MVP
+	Ticket       string                  `json:"ticket,omitempty"`
 	Participants map[string]*Participant `json:"participants,omitempty"`
+	Votes        map[string]*Vote        `json:"votes,omitempty"`
 }
